83cd28c07407def7e4a41e9027c7f4fd93d9425f
georgiy2003 <urumyangaa@gmail.com>
2025-11-30 17:13:23 +0300

    Update views.py

--- ORM1/work_with_database/phones/views.py
+++ ORM1/work_with_database/phones/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Phone
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    sort = request.GET.get('sort')
+
+    phones = Phone.objects.all()
+
+    if sort == 'name':
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+
+    context = {
+        'phones': phones,
+        'sort': sort
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone = get_object_or_404(Phone, slug=slug)
+
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
+
